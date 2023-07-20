95340e76651b026d39ed27f5586985f98222f6db
aryark <aryark3112@gmail.com>
2023-07-20 09:33:08 +0530

    first commit

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace _20July
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        public Employee()
+        { 
+        
+            Console.WriteLine("Parameterized Constructor Call");
+            id = -1;
+            name = "not given";
+        }
+        public Employee(int id,string name)
+        {
+            Console.WriteLine("Parameterized Constructor");
+            this.id = id;
+            this.name = name;
+        }
+        public void Register()
+        {
+            Console.WriteLine("Enter Id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID: \t"+id);
+            Console.WriteLine("Name:\t"+name);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _20July
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Display();
+            Employee employee2 = new Employee(12, "Sam");
+            employee2.Display();
+            Employee emp3 = new Employee();
+            emp3.Register();
+            emp3.Display();
+            Console.ReadKey();
+        }
+    }
+}
